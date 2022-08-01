db6e73ec94de2ca22b33160011d6136c02a6d726
Dennish02 <dennishesler02@gmail.com>
2022-08-01 16:13:14 -0300

    performance 2

--- next.config.js
+++ next.config.js
@@ -14,10 +14,10 @@ const nextConfig = {
 }
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-  openAnalyzer: true,
+ 
 })
 module.exports = {
-  compress: true,
+  compress: false,
 }
 module.exports = withBundleAnalyzer({})
 module.exports = nextConfig
